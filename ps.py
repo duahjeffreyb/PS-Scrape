28f49d5d3fbd088dd26eb3cdeb917e0bcbc147de
Jeffrey <duahjeffrey08@gmail.com>
2020-08-02 18:39:42 -0400

    added a column to show if game was recently added

--- ps.py
+++ ps.py
@@ -1,5 +1,5 @@
 ##############################################
-#YOU WERE THINKING OF MOVING THINGS TO FUCNTIONS LIKE A READFILE FUNCTION AND A CHECK IF NEW FUNCTION
+# YOU WERE THINKING OF MOVING THINGS TO FUCNTIONS LIKE A READFILE FUNCTION AND A CHECK IF NEW FUNCTION
 ##############################################
 from bs4 import BeautifulSoup as soup
 #from apschedule.schedulers.blocking import BlockingSchedule
@@ -8,51 +8,76 @@ import schedule
 import time
 
 
-def psGames():
-    pastNewGames = []
-    url = requests.get('https://store.playstation.com/en-us/grid/STORE-MSF77008-NEWGAMESGRID/1').text
+url = requests.get(
+    'https://store.playstation.com/en-us/grid/STORE-MSF77008-NEWGAMESGRID/1').text
+gameTitle = 'gameTitles.txt'
 
-    gameTitle = 'gameTitles.txt'
-    with open(gameTitle, 'r', encoding='utf-8') as reader:
-        titlesInFile = reader.readlines()
-        for title in titlesInFile:
-            pastNewGames.append(title.strip())
-        print(pastNewGames)
-        reader.close()
 
-    file = open(gameTitle, 'w', encoding='utf-8')
+def getPsGames():
+    b_soup = soup(url, 'lxml')
+    titles = b_soup.findAll('div', {'class': 'grid-cell__title'})
+    allPrices = b_soup.findAll(
+        'div', {'class': '__shared-presentation__price-display__900cc'})
 
+    pastGames = getPastTitles()
+    print(pastGames)
 
     filename = 'newGames.csv'
-    f = open(filename,'w', encoding='utf-8')
-    headers = "title, price\n"
+    f = open(filename, 'w', encoding='utf-8')
+    headers = "Title, Price, Newly Added?\n"
     f.write(headers)
-
-    b_soup = soup(url, 'lxml')
-    titles = b_soup.findAll('div', {'class':'grid-cell__title'})
-    allPrices = b_soup.findAll('div', {'class':'__shared-presentation__price-display__900cc'})
 
     for i in range(0, len(titles)):
         price = allPrices[i].h3.text
         gameTitle = titles[i].span.text
+        if(checkIfNew(pastGames, titles[i].text.strip()) == True):
+            f.write(gameTitle.replace(",", " ") +
+                    ',' + price + ',' + 'new' + '\n')
+        else:
+            f.write(gameTitle.replace(",", " ") +
+                    ',' + price + ',' + "-" + '\n')
         print(gameTitle)
         print(price)
-        file.write(gameTitle + '\n')
-        f.write(gameTitle.replace(","," ") + ',' + price + '\n')
+        print()
     f.close()
+    getCurrentTitles(titles)
+    # print(page_soup)
+    # print(b_soup.prettify())
+    # print(price)
+
+
+# schedule.every().tuesday.at('15:25').do(psGames)
+
+
+# while 1:
+    # schedule.run_pending()
+    # time.sleep(1)
+def checkIfNew(pastNewGames, title):
+    if(len(pastNewGames) == 0):
+        return True
+    j = 0
+    if title in pastNewGames:
+        return False
+    else:
+        return True
+
+
+def getPastTitles():
+    games = []
+    with open(gameTitle, 'r', encoding='utf-8') as reader:
+        titlesInFile = reader.readlines()
+        for title in titlesInFile:
+            games.append(title.strip())
+        reader.close()
+        return games
+
+
+def getCurrentTitles(titles):
+    file = open('gameTitles.txt', 'w', encoding='utf-8')
+    for title in titles:
+        file.write(title.span.text + '\n')
     file.close()
-    #print(page_soup)
-    #print(b_soup.prettify())
-    #print(price)
-
-
-#schedule.every().tuesday.at('15:25').do(psGames)
-
-
-#while 1:
-    #schedule.run_pending()
-    #time.sleep(1)
 
 
 if __name__ == "__main__":
-    psGames()
+    getPsGames()
